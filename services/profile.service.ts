931dffe62d33c806b44fc9d50a71c73783ac5208
Winston <wagawc@gmail.com>
2019-01-22 07:41:30 +0300

    create official app structure with components and services

---
+++ services/profile.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+private username: string;
+private clientid = 'b9fcec2b08992757b907';
+private clientsecret='20ee09e7af46a2bcbe727a3a828716e0659269a2';
+
+
+
+  constructor(private http:Http) {
+    console.log("service is now ready!");
+    this.username = 'cinston';
+   }
+getProfileInfo(){
+  return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+ }
+
+}
